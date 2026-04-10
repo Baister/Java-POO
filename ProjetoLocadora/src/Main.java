662616a15553f705c704978e12a1be86e80015ba
Gustavo <gustavobaister@gmail.com>
2026-04-10 20:18:29 -0300

    feat Criação main

---
+++ ProjetoLocadora/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("Ferrari", "PORTOFINO", 2026, "ghf-888", 2000000, 2);
+        System.out.println(carro1.toString());
+
+    }
+}
