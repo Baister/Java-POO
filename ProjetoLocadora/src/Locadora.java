1c0649307cf6cf02825886b5587871fb1fef91db
Gustavo <gustavobaister@gmail.com>
2026-04-14 20:13:03 -0300

    feat criacao metodo aluguel veiculo

--- ProjetoLocadora/src/Locadora.java
+++ ProjetoLocadora/src/Locadora.java
@@ -14,10 +14,22 @@ public class Locadora {
     public void listarVeiculo(){
         for (int i = 0; i< frota.size(); i++){
             Veiculo v = frota.get(i);
-            System.out.printf("[%d] %s %s | Diária: R$%.2f\n", (i+1), v.getMarca(), v.getModelo(), v.getPlaca(), v.getPrecoBase());
+            System.out.printf("[%d] %s %s %s | Diária: R$%.2f \n", (i+1), v.getMarca(), v.getModelo(), v.getPlaca(), v.calcularDiaria());
         }
         System.out.println("===========================================");
     }
+
+    public void aluguelVeiculo(int dias, int idVeiculo){
+        //Logica para alugar o carro.
+        for(int i = 0; i < frota.size(); i++){
+            if(frota.indexOf(frota.get(i)) == idVeiculo){
+                System.out.println("Veiculo encontrado!");
+                System.out.println("Para o veiculo: " + frota.get(i).toString());
+                System.out.printf("Preco base: R$%.2f | Dias alugados: %s| Novo preco: R$%.2f", frota.get(i).calcularDiaria(), dias, frota.get(i).calcularDiaria() * dias);
+            }
+        }
+    }
+
     public void alugarVeiculo(String modelo){
         for (Veiculo v:frota){
             if(v.getModelo().equalsIgnoreCase(modelo)){
--- ProjetoLocadora/src/Main.java
+++ ProjetoLocadora/src/Main.java
@@ -7,10 +7,11 @@ public class Main {
 //        System.out.println(moto1.toString());
 //
         Locadora locadora = new Locadora();
-        locadora.adicionarVeiculo(new Carro("Ferrari", "PORTOFINO", 2026, "ghf-888", 10000, 4));
+        locadora.adicionarVeiculo(new Carro("Ferrari", "PORTOFINO", 2026, "ghf-888", 5000, 4));
         locadora.adicionarVeiculo(new Moto("Harley-Davidson", "FATBOY", 2026, "png-666", 1000, 1600));
         locadora.listarVeiculo();
-        locadora.alugarVeiculo("fatboy");
+
+        locadora.aluguelVeiculo(2, 0);
     }
 
 }
